df78fbe0686f37551c40fec5075aa4c99ec97761
TheL0br1 <82335893+TheL0br1@users.noreply.github.com>
2023-11-24 15:45:34 +0200

    1.13

--- main.c
+++ main.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 #include<stdbool.h>
+#include <stdlib.h>
+
+#define  MAXLENGTH 100
+void printGisto(int *gisto){
+    for (int i = 0; i < MAXLENGTH; ++i) {
+        if(gisto[i]==0) continue;
+        printf("%d: ", i);
+        for (int j = 0; j < gisto[i]; ++j) {
+            printf("-");
+        }
+        printf("\n");
+    }
+}
 int main()
 {
+    int gisto[MAXLENGTH] = {0};
     char c;
-    bool flag = true;
+    bool flag = false;
+    int count =0;
     while((c=getchar())!=EOF){
         switch (c) {
             case ' ':
                 if(flag) {
-                    printf("\n");
+                    gisto[count]++;
+                    count = 0;
                 }
                 flag = false;
                 break;
             default:
-                printf("%c", c);
+                count++;
                 flag = true;
                 break;
         }
     }
-
+    printGisto(gisto);
+    system("PAUSE");
 
     return 0;
 }
