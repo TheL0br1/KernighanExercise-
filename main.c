e5b0ceffacfb3914d8ccd925953c547986e13a6a
TheL0br1 <82335893+TheL0br1@users.noreply.github.com>
2023-11-24 11:14:09 +0200

    1.3

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("\c");
-//error "Unknown escape sequence '\c'"
-    return 0;
+int main()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+    lower = 0; /* нижня межа температурної шкали */
+    upper = 300; /* верхня межа */
+    step = 20; /* розмiр кроку */
+    fahr = lower;
+    printf("From Fahrenheit to Celsius\n");
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }
