d6152aa8722ac1babc5ff90d35dfef3c7206c429
TheL0br1 <82335893+TheL0br1@users.noreply.github.com>
2023-11-24 11:17:35 +0200

    1.4

--- main.c
+++ main.c
@@ -6,12 +6,12 @@ int main()
     float lower, upper, step;
     lower = 0; /* нижня межа температурної шкали */
     upper = 300; /* верхня межа */
-    step = 20; /* розмiр кроку */
-    fahr = lower;
-    printf("From Fahrenheit to Celsius\n");
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+    step = 5; /* розмiр кроку */
+    celsius = lower;
+    printf("From Celsius to Fahrenheit\n");
+    while (celsius <= upper) {
+        fahr = ((9.0/5.0) * celsius)+32.0;
+        printf("%3.0f %6.1f\n", celsius,fahr);
+        celsius += step;
     }
 }
