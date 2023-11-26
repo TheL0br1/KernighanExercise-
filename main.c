046ba13248d7ddb37de0dd93fe4b032f8da6dcbd
Artem <L0br1Artem@gmail.com>
2023-11-26 22:58:16 +0200

    2.2

--- main.c
+++ main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-#include <limits.h>
-#include<float.h>
+#include<stdlib.h>
 int main() {
     // Для знакових типів
-    printf("Signed char: [%d, %d]\n", SCHAR_MIN, SCHAR_MAX);
-    printf("Signed short: [%d, %d]\n", SHRT_MIN, SHRT_MAX);
-    printf("Signed int: [%d, %d]\n", INT_MIN, INT_MAX);
-    printf("Signed long: [%ld, %ld]\n", LONG_MIN, LONG_MAX);
+    int i=0;
+    int lim = 10;
+    char c;
+    char s[10];
+    while((c=getchar()) != EOF) {
+        if (i > (lim - 1)) break;
+        if (c == '\n') break;
 
-    // Для беззнакових типів
-    printf("Unsigned char: [0, %u]\n", UCHAR_MAX);
-    printf("Unsigned short: [0, %u]\n", USHRT_MAX);
-    printf("Unsigned int: [0, %u]\n", UINT_MAX);
-    printf("Unsigned long: [0, %lu]\n", ULONG_MAX);
-    printf("Float: [%e, %e]\n", FLT_MIN, FLT_MAX);
-    printf("Double: [%e, %e]\n", DBL_MIN, DBL_MAX);
-    printf("Long Double: [%Le, %Le]\n", LDBL_MIN, LDBL_MAX);
+        s[i] = c;
+        i ++;
+    }
+    s[i] = '\0';
+    printf("%s\n", s);
     return 0;
 }
