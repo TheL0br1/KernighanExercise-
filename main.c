2cd9658165d739efec803875652e88e1ddd16674
Artem <L0br1Artem@gmail.com>
2023-11-26 09:43:09 +0200

    1.16 unlimited string input

--- main.c
+++ main.c
@@ -1,24 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
+long long getline(char *p);
+void copy(char to[], char from[]);
+/* виводить найдовший рядок вводу */
+int main() {
+    long long max; /* найбiльша знайдена довжина */
 
-#include <stdio.h>
-double f2cConv(double fahr){
-    return (5.0/9.0) * (fahr-32.0);
-}
-double c2fConv(double cels){
-    ((9.0/5.0) * cels)+32.0;
-}
-int main()
-{
-    float fahr, celsius;
-    float lower, upper, step;
-    lower = 0; /* нижня межа температурної шкали */
-    upper = 300; /* верхня межа */
-    step = 5; /* розмiр кроку */
-    celsius = lower;
-    printf("From Celsius to Fahrenheit\n");
-    while (celsius <= upper) {
-        fahr = c2fConv(celsius);
-        printf("%3.0f %6.1f\n", celsius,fahr);
-        celsius += step;
+    char *longest = malloc(20); /* найдовший рядок буде збережено тут */
+    max = 0;
+    while (1) {
+        long long i = 0;
+        long long size = 20;
+        char *line = malloc(size); /* поточний рядок вводу */
+        while ((line[i] = getchar()) != EOF && line[i] != '\n') {
+
+            i++;
+            if (i >= size) {
+                size *= 2;
+                line = realloc(line, size);
+                if (line == NULL) {
+                    break;
+                }
+            }
+        }
+        if(line[i]==EOF){
+            break;
+        }
+        line[i] = '\0';
+        printf("String = %s, Address: %u\n", line, line);
+        if (i > max) {
+            max = i;
+            longest = malloc(i);
+            printf("temp line: %s, Address: %u\n", line, line);
+            copy(longest, line);
+        }
+        free(line);
+
     }
+
+    if (max > 0) /* було знайдено рядок */
+        printf("Longest String: %s", longest);
+    free(longest);
+    return 0;
+}
+/* getline: зчитує рядок у s, повертає довжину */
+long long getline(char *p)
+{
+    long long size = 20;
+    long long i = 0;
+    while((p[i]= getchar())!=EOF && p[i]!='\n'){
+        i++;
+        if(i>=size){
+            size*=2;
+            p = realloc(p, size);
+            if(p==NULL){
+                return -1;
+            }
+        }
+    }
+    p[i]='\0';
+    printf("String = %s, Address: %u\n", p, p);
+    return i;
+}
+/* copy: копiює from до to; припускає, що to досить великий */
+void copy(char to[], char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+    ++i;
 }
