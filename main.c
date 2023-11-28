6ca26743ca2a76dc6754007fbc55ec6e4b1f9c63
Artem <L0br1Artem@gmail.com>
2023-11-28 11:00:03 +0200

    2.7

--- main.c
+++ main.c
@@ -8,21 +8,20 @@ void printbitset(int x, int n) {
 }
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
     unsigned mask = ~(~0 << n) << (p - n + 1);
-
     x = x & ~mask;
-
     y = (y & ~(~0 << n)) << (p - n + 1);
-
     return x | y;
 }
-
+unsigned invert(unsigned x, int p, int n){
+    unsigned mask = ((x >> (p+1-n)) & ~(~0 << n))<<(p-1);
+    printbitset(mask,8);
+    return x ^ mask;
+}
 
 int main() {
-    unsigned int a = 30;
-    unsigned int b = 2;
+    unsigned int a = 53;
     printbitset(a,8);
-    printbitset(b, 8);
-    printbitset(setbits(a,2,2,b),8);
+    printbitset(invert(a,5,2),8);
 
     return 0;
 }
