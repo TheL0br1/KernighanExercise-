51e20f19cdfcae98660b350e707a80fe299aa456
Artem <L0br1Artem@gmail.com>
2023-11-28 12:02:58 +0200

    2.10

--- main.c
+++ main.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-#include<stdbool.h>
-//Якщо число парне, його крайній правий
-// біт буде 1, інакше 0. Виразом (x-1) ми
-// змінюємо його парність і або крайній біт
-// встановлюємо з 1 до 0, або рухаємо усі послідовність вправо
-int bitcount(unsigned x) {
-    int count = 0;
-    while (x != 0) {
-        x &= (x - 1);
-        count++;
-    }
-    return count;
+
+int lower(int c)
+{
+    int res;
+    (c >= 'A' && c <= 'Z') ?  (res= (c + 'a' - 'A')) : (res = c);
+    return res;
 }
+
 int main() {
-    unsigned int a = 53;
-    printf("%d", bitcount(a));
+    printf("%c", lower((int) 'A'));
 
     return 0;
 }
