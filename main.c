df7141867ac046687c554b632bc30b2ef40f7287
Artem <L0br1Artem@gmail.com>
2023-11-28 11:56:07 +0200

    2.9

--- main.c
+++ main.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 #include<stdbool.h>
-void printbitset(int x, int n) {
-    for (int i = n - 1; i >= 0; i--) {
-        printf("%d", x & (1 << i) ? 1 : 0);
+//Якщо число парне, його крайній правий
+// біт буде 1, інакше 0. Виразом (x-1) ми
+// змінюємо його парність і або крайній біт
+// встановлюємо з 1 до 0, або рухаємо усі послідовність вправо
+int bitcount(unsigned x) {
+    int count = 0;
+    while (x != 0) {
+        x &= (x - 1);
+        count++;
     }
-    printf("\n");
-}
-unsigned setbits(unsigned x, int p, int n, unsigned y) {
-    unsigned mask = ~(~0 << n) << (p - n + 1);
-    x = x & ~mask;
-    y = (y & ~(~0 << n)) << (p - n + 1);
-    return x | y;
-}
-unsigned invert(unsigned x, int p, int n){
-    unsigned mask = ((x >> (p+1-n)) & ~(~0 << n))<<(p-1);
-    printbitset(mask,8);
-    return x ^ mask;
-}
-unsigned rightpot(unsigned x, unsigned n){
-    return x >> n;
+    return count;
 }
 int main() {
     unsigned int a = 53;
-    printbitset(a,8);
-    printbitset(rightpot(a,3),8);
+    printf("%d", bitcount(a));
 
     return 0;
 }
