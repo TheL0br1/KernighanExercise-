11e770e5b9209d9421b6a09851992e61c9dbfe2e
Artem <L0br1Artem@gmail.com>
2023-11-29 11:56:08 +0200

    3.3 the case with the '-' sign at the beginning or end is not implemented, i didn't get how it should look like

--- main.c
+++ main.c
@@ -1,57 +1,24 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
-char* escape(char s[], char t[]){
-    int i =0;
-    int j =0;
-    while(s[i]!='\0'){
-        switch (s[i]) {
-            case '\n':
-                t[j++] = '\\';
-                t[j++] = 'n';
-                break;
-            case '\t':
-                t[j++] = '\\';
-                t[j++] = 't';
-                break;
-            default:
-                t[j++] = s[i];
+char* expand(char s1[], char s2[]){
+    int i  =0;
+    int t =0;
+    while(s1[i]!='\0'){
+        if(s1[i+1]=='-'){
+            for (int j = s1[i]; j <= s1[i+2]; ++j, ++t) {
+                s2[t] = (char)j;
+            }
+            s2[t] = '\0';
         }
         i++;
     }
-    t[j] = '\0';
-    printf("%s", t);
-    return t;
+    printf("%s", s2);
+    return s2;
 }
-char* antiescape(char s[], char t[]){
-    int i =0;
-    int j =0;
-    while(s[i]!='\0'){
-        char temp[2] = {s[i],s[i+1]};
-        switch (temp[0]*temp[1]) {
-            case '\\' * 'n':
-                t[j++] = '\n';
-                i++;
-                break;
-            case '\\'*'t':
-                t[j++] = '\t';
-                i++;
-                break;
-            default:
-                t[j++] = s[i];
-
-        }
-        i++;
-    }
-    t[j] = '\0';
-    printf("%s", t);
-    return t;
-}
-
 int main() {
-    char a[20] ="\\tHello\\nWorld\\t\0";
-    char b[20];
-
-    antiescape(a,b);
+    char a[20] ="a-z0-9\0";
+    char b[100];
+    expand(a,b);
     return 0;
 }
