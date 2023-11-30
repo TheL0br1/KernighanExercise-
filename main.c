c03c6482519d46e0b46e0cfcf5341841b35b4230
Artem <L0br1Artem@gmail.com>
2023-11-30 15:41:17 +0200

    4.2

--- main.c
+++ main.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 #include<limits.h>
 #include<string.h>
-
-
-int strindex(char s[], char t[]) {
-    int i, j, k, lastMatch = -1;
-
-    for (i = 0; s[i] != '\0'; i++) {
-        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
-            ;
-
-        if (t[k] == '\0') {
-            lastMatch = i;
+#include<ctype.h>
+#include<math.h>
+double MYatof(char s[])
+{
+    double val=1, power=1;
+    int exp=0, expSign =1;
+    int i=0, sign;
+    for (i = 0; isspace(s[i]); i++) { /* пропустити пробiли */
+        ;
+    }
+ sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-') {
+        i++;
+    }
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.') {
+        i++;
+    }
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    if(s[i] == 'e'){
+        i++;
+        if(s[i]=='-'){
+            i++;
+            expSign=-1;
         }
     }
-
-    return lastMatch;
+    for (; isdigit(s[i]); i++) {
+        exp = 10.0 * exp + (s[i] - '0');
+    }
+    printf("%c", s[i]);
+    return sign * val / power * pow(10,expSign*exp);
 }
 
 int main() {
-    printf("%d\n", strindex("hello world", "wo"));
-    printf("%d\n", strindex("hello world", "ow"));
+    printf("%f", MYatof("223.333e3\0"));
 
     return 0;
 }
