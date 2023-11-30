536d944fc2103d3f57f6c6f5a7435271a981963b
Artem <L0br1Artem@gmail.com>
2023-11-30 21:00:15 +0200

    4.8

--- getch.h
+++ getch.h
@@ -11,11 +11,11 @@ char buf[BUFSIZE]; /* буфер для ungetch */
 int bufp = 0; /* наступна вiльна позицiя у buf */
 int getch(void) /* отримати (можливо виштовхнутий) знак */
 {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+    return (bufp == 1) ? buf[--bufp] : getchar();
 }
 void ungetch(int c) /* виштовхнути знак назад у ввiд */
 {
-    if (bufp >= BUFSIZE)
+    if (bufp >= 1)
         printf("ungetch: too many characters\n");
     else
         buf[bufp++] = c;
