3c9b8923cc8429304075743f8d6f1f22c3deb4c7
Artem <L0br1Artem@gmail.com>
2023-11-30 10:23:37 +0200

    4.1

--- main.c
+++ main.c
@@ -1,47 +1,26 @@
 #include <stdio.h>
 #include<limits.h>
 #include<string.h>
-void reverse(char s[])
-{
-    int c, i, j;
-    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
+
+
+int strindex(char s[], char t[]) {
+    int i, j, k, lastMatch = -1;
+
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+            ;
+
+        if (t[k] == '\0') {
+            lastMatch = i;
+        }
     }
-}
-void itoa(int n, char s[], int b)
-{
-    int min_int = 0;
-    int i, sign;
-    if (INT_MIN == n) {
-        min_int = 1;
-        n++;
-    }
-    if ((sign = n) < 0) /* виявити знак */
-        n = -n; /* перетворити n на додатнє */
-    i = 0;
-    do { /* генерувати цифри в зворотньому порядку */
-        s[i++] = n % 10 + '0'; /* одержати наступну цифру */
-    } while ((n /= 10) > 0 && i<b); /* видалити її */
-    if (min_int) {
-        s[0] = (char) (s[0] + 1);
-        s[i++] = '-';
-    }
-    if(sign<0){
-        s[i++]= '-';
-    }
-    while(i<b){
-        s[i++] = ' ';
-    }
-    s[i] = '\0';
-    reverse(s);
+
+    return lastMatch;
 }
 
 int main() {
-    int a = 12000;
-    char b[100];
-    itoa(a,b,7);
-    printf("%s", b);
+    printf("%d\n", strindex("hello world", "wo"));
+    printf("%d\n", strindex("hello world", "ow"));
+
     return 0;
 }
