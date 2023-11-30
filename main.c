edd3081ec7842cacad731a4ae22385b6533661e6
Artem <L0br1Artem@gmail.com>
2023-11-30 09:05:40 +0200

    3.4 fixed

--- main.c
+++ main.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include<limits.h>
 #include<string.h>
-char* expand(char s1[], char s2[]){
-    int i  =0;
-    int t =0;
-    while(s1[i]!='\0'){
-        if(s1[i+1]=='-'){
-            for (int j = s1[i]; j <= s1[i+2]; ++j, ++t) {
-                s2[t] = (char)j;
-            }
-            s2[t] = '\0';
-        }
-        i++;
+void reverse(char s[])
+{
+    int c, i, j;
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
     }
-    printf("%s", s2);
-    return s2;
 }
+void itoa(int n, char s[])
+{
+    int min_int = 0;
+    int i, sign;
+    if (INT_MIN == n) {
+        min_int = 1;
+        n++;
+    }
+    if ((sign = n) < 0) /* виявити знак */
+        n = -n; /* перетворити n на додатнє */
+    i = 0;
+    do { /* генерувати цифри в зворотньому порядку */
+        s[i++] = n % 10 + '0'; /* одержати наступну цифру */
+    } while ((n /= 10) > 0); /* видалити її */
+    if (min_int) {
+        s[0] = (char) (s[0] + 1);
+        s[i++] = '-';
+    }
+    if(sign<0){
+        s[i++]= '-';
+    }
+    s[i] = '\0';
+    reverse(s);
+}
+
 int main() {
-    char a[20] ="a-z0-9\0";
+    int a = 0;
     char b[100];
-    expand(a,b);
+    itoa(a,b);
+    printf("%s", b);
     return 0;
 }
