b09091989c44c2a7b6702066299c3a9cd96cb17e
Artem <L0br1Artem@gmail.com>
2023-12-03 00:12:50 +0200

    4.13

--- main.c
+++ main.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
+#include<string.h>
 
-void itoa(int n, char s[], int* i)
-{
-    if (n < 0) {
-        s[(*i)++] = '-';
-        n = -n;
+void reverse(char s[], int a, int b){
+    if(a<b){
+        char temp = s[a];
+        s[a] = s[b];
+        s[b] = temp;
+        reverse(s, a+1, b-1);
     }
-
-    if (n / 10) {
-        itoa(n / 10, s, i);
-    }
-
-    s[(*i)++] = n % 10 + '0';
-    s[*i] = '\0';
 }
-
 int main() {
-    int num = 1023352;
-    char result[20];
-    int i = 0;
+    char s[] = "i must get reversed!";
+    printf("%s\n", s);
+    reverse(s,0, strlen(s)-1);
+    printf("%s\n", s);
 
-    itoa(num, result, &i);
-
-    printf("Int: %d\nStr: %s\n", num, result);
 
     return 0;
 
