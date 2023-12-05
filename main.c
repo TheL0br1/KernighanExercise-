91de3c6874070e9e3b05dc509f3b09a2daca1299
Artem <L0br1Artem@gmail.com>
2023-12-05 16:16:12 +0200

    5.3

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-#include "stack.h"
-#include "getint.h"
+#include<stdlib.h>
 #define BUFSIZE 500 /* максимальний розмiр операнда або оператора */
-
+void strcat1(char *s, char *t)
+{
+    int i, j;
+    i = j = 0;
+    while (*(s+i) != '\0') { /* знаходить кiнець s */
+        i++;
+    }
+    while ((*(s+i) = *(t+j)) != '\0') { /* копiює t */
+        i++; j++;
+    }
+}
 int main() {
-    float a[100];
-    printf("%d\n", getfloat(&a[0]));
-    printf("%f", a[0]);
+    char s[]="frkjlk\0";
+    char t[]= "00000\0";
+    strcat1(s,t);
+    printf("%s", s);
     return 0;
 }
