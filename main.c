ef1fe178c49fd72dfde15a130617cd50da0e69c7
Artem <L0br1Artem@gmail.com>
2023-12-05 18:28:20 +0200

    5.5

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
-#include<stdlib.h>
-#define BUFSIZE 500 /* максимальний розмiр операнда або оператора */
-int strend(char* s, char* t){
-    int i=0,j=0;
-    while(s[i] != t[0]){
+
+void strncpy1(char*s,char*t, int n){
+    int i =0;
+    while(t[i]!='\0' && i<n){
+        s[i] = t[i];
         i++;
     }
-    while(s[i]!='\0'){
-        if(s[i] != t[j++]){
+    s[i] = '\0';
+}
+void strncat1(char *s, char *t, int n){
+    int i =0, j=0;
+    while(s[i++]!='\0'){;}
+    i--;
+    while ((s[i++] = t[j++]) != '\0' && j < n) {
+        ;
+    }
+}
+int strncmp1(char *s, char*t, int n){
+    int i =0;
+    while(s[i]!='\0' && t[i]!='\0' && i<n){
+        if(s[i]!=t[i]){
             return 0;
         }
         i++;
     }
     return 1;
+
+
 }
 int main() {
-    char s[]="frkjlk0000\0";
-    char t[]= "000\0";
-    int a = strend(s,t);
-    printf("%d", a);
+  ;
     return 0;
 }
