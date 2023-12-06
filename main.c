8fe224ab9fe7f71ab9dddbbe39472c6f2459577d
Artem <L0br1Artem@gmail.com>
2023-12-06 22:56:39 +0200

    5.14

--- main.c
+++ main.c
@@ -1,53 +1,75 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int getline(char **p) {
-    long long size = 20;
-    *p = malloc(size);
-    long long i = 0;
-    int c;
+#include <string.h>
+#include <malloc.h>
+#include "readlines.h"
+#define MAXLINES 5000
+int numeric = 0;
+int reverse = 0;
 
-    if (*p == NULL) {
-        return -1;
-    }
+char *lineptr[MAXLINES]; /* покажчики на текст рядкiв */
+void qsort1(void *lineptr[], int left, int right,
+           int (*comp)(const void *, const void *));
+int numcmp(char *, char *);
+/* сортує рядки вводу */
+int main(int argc, char *argv[])
+{
+    int nlines; /* кiлькiсть прочитаних рядкiв вводу */
 
-    while ((c = getchar()) != EOF && c != '\n') {
-        (*p)[i] = c;
-        i++;
-        if (i >= size) {
-            size *= 2;
-            *p = realloc(*p, size);
-            if (*p == NULL) {
-                return -1;
-            }
+    if (argc > 1 )
+        if(strcmp(argv[1], "-n") == 0) {
+            numeric = 1;
         }
+        if(strcmp(argv[1], "-r") == 0) {
+            reverse = 1;
+        }
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        qsort1((void**) lineptr, 0, nlines-1,
+              (int (*)(const void*,const void*))(numeric ? numcmp : strcmp));
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("input too big to sort\n");
+        return 1;
     }
-    if(c==EOF){
-        return -1;
-    }
-    (*p)[i] = '\0';
-    printf("String = %s, Address: %p\n", *p, (void *)*p);
-    return i;
 }
-int main(int argc, char*argv[]) {
-    int n = 10;
-    int i = 0;
-    if(argc>1){
-        n = atoi(argv[1]);
 
-    }
-    char **s = malloc(n * sizeof(char *));
-    while (getline(&(s[i]))!=-1){
-        printf("%s\n", s[i]);
-
-        i++;
-        if(i>=(n+1)){
-            i=0;
+int numcmp(char *s1, char *s2)
+{
+    double v1, v2;
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if (v1 < v2)
+        return -1;
+    else if (v1 > v2)
+        return 1;
+    else
+        return 0;
+}
+void swap(void *v[], int i, int j)
+{
+    void *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+void qsort1(void *v[], int left, int right,
+           int (*comp)(const void *, const void *))
+{
+    int i, last;
+    void swap(void *v[], int, int);
+    if (left >= right) /* не робить нiчого, якщо масив мiстить */
+    return; /* менше нiж два елементи */
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++)
+        if(reverse == 0) {
+            if ((*comp)(v[i], v[left]) < 0)
+                swap(v, ++last, i);
+        }else{
+            if ((*comp)(v[i], v[left]) < 0)
+                swap(v, i,++last);
         }
-    }
-    n = (n>i) ? i:n;
-    for (int j = 0; j < n; ++j) {
-        printf("%s\n", s[j]);
-    }
-    return 0;
+    swap(v, left, last);
+    qsort(v, left, last-1, comp);
+    qsort(v, last+1, right, comp);
 }
