01e46ff0ff67df3a5d4d1106b17c6cf9d73f61a9
Artem <L0br1Artem@gmail.com>
2023-12-06 21:50:30 +0200

    5.13; 5.11, 5.12 skip. Problem statement unclear

--- main.c
+++ main.c
@@ -1,69 +1,53 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<ctype.h>
-#include "stack.h"
-#define NUMBER '0'
-int getNext(char* a){
-    int i = 0;
-    int size = strlen(a);
-    while(a[i] == ' ' || a[i] == '\t'){ i++;}
-    if(a[i]== '-' && isdigit(a[i+1])){
-        return NUMBER;
-    }
-    if(!isdigit(a[i]) ){
-        return a[i];
-    }
-    else{
-        return NUMBER;
-    }
+int getline(char **p) {
+    long long size = 20;
+    *p = malloc(size);
+    long long i = 0;
+    int c;
 
-}
-int main(int argc, char* argv[]) {
-
-    if(argc<=1){
+    if (*p == NULL) {
         return -1;
     }
-    for (int i = 0; i <argc; ++i) {
-        printf("%s\n", argv[i]);
-    }
-    int type = 0;
-    int op2;
-    for (int i = 1; i <argc; ++i) {
-        type = getNext(argv[i]);
-        switch (type) {
-            case NUMBER:
-                push(atof(argv[i]));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
 
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0) {
-                    push(pop() / op2);
-                }
-
-                else
-                    printf("error: zero divisor\n");
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            default:
-                printf("error: unknown command %s\n", argv[i]);
-                break;
+    while ((c = getchar()) != EOF && c != '\n') {
+        (*p)[i] = c;
+        i++;
+        if (i >= size) {
+            size *= 2;
+            *p = realloc(*p, size);
+            if (*p == NULL) {
+                return -1;
+            }
         }
     }
-    printf("\t%.8g\n", pop());
+    if(c==EOF){
+        return -1;
+    }
+    (*p)[i] = '\0';
+    printf("String = %s, Address: %p\n", *p, (void *)*p);
+    return i;
+}
+int main(int argc, char*argv[]) {
+    int n = 10;
+    int i = 0;
+    if(argc>1){
+        n = atoi(argv[1]);
 
+    }
+    char **s = malloc(n * sizeof(char *));
+    while (getline(&(s[i]))!=-1){
+        printf("%s\n", s[i]);
+
+        i++;
+        if(i>=(n+1)){
+            i=0;
+        }
+    }
+    n = (n>i) ? i:n;
+    for (int j = 0; j < n; ++j) {
+        printf("%s\n", s[j]);
+    }
     return 0;
 }
