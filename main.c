9b10d8d6de157c9a80341d305f61664053e19945
Artem <L0br1Artem@gmail.com>
2023-12-07 18:34:05 +0200

    5.19

--- main.c
+++ main.c
@@ -16,14 +16,32 @@ char out[1000];
 
 int main() /* перетворює оголошення на словесний опис */
 {
-    while (gettoken() != EOF) { /* перша лексема рядка */
-        strcpy(datatype, token); /* is the datatype */
-        out[0] = '\0';
-        dcl(); /* читання решти рядка */
-        if (tokentype != '\n') {
-            printf("syntax error\n");
+    int type;
+    int prev_type=-1;
+    char temp[MAXTOKEN];
+    while (gettoken() != EOF) {
+        strcpy(out, token);
+        while ((type = gettoken()) != '\n') {
+            if (type == PARENS || type == BRACKETS) {
+                strcat(out, token);
+            }
+            else if (type == '*') {
+                if(prev_type == BRACKETS){
+                    sprintf(temp, "*%s", out);
+
+                }
+                else {
+                    sprintf(temp, "(*%s)", out);
+                }
+                strcpy(out, temp);
+            } else if (type == NAME) {
+                sprintf(temp, "%s %s", token, out);
+                strcpy(out, temp);
+            } else
+                printf("invalid input at %s\n", token);
+            prev_type = type;
         }
-        printf("%s: %s %s\n", name, out, datatype);
+        printf("%s\n", temp);
     }
     return 0;
 }
@@ -31,8 +49,8 @@ int gettoken(void) /* return next token */
 {
     int c;
     char *p = token;
-    while ((c = getch()) == ' ' || c == '\t')
-    ;
+    while ((c = getch()) == ' ' || c == '\t') { ;
+    }
     if (c == '(') {
         if ((c = getch()) == ')') {
             strcpy(token, "()");
@@ -42,19 +60,22 @@ int gettoken(void) /* return next token */
             return tokentype = '(';
         }
     } else if (c == '[') {
-        for (*p++ = c; (*p++ = getch()) != ']'; )
-        ;
+        for (*p++ = c; (*p++ = getch()) != ']'; ) {
+            ;
+        }
         *p = '\0';
         return tokentype = BRACKETS;
     } else if (isalpha(c)) {
-        for (*p++ = c; isalnum(c = getch()); )
+        for (*p++ = c; isalnum(c = getch());) {
             *p++ = c;
+        }
         *p = '\0';
         ungetch(c);
         return tokentype = NAME;
 
-    } else
+    } else {
         return tokentype = c;
+    }
 }
 void dcl(void)
 {
