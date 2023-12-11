e6bd0bcd7b7057420361abde80dfe412489499be
Artem <L0br1Artem@gmail.com>
2023-12-11 10:06:13 +0200

    6.6

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include<string.h>
 #include<malloc.h>
-#define HASHSIZE 5
+#include <ctype.h>
+#include "getch.h"
+#define HASHSIZE 101
+#define MAXWORD 101
 struct nlist { /* запис таблицi: */
     struct nlist *next; /* наступний запис в ланцюжку */
     char *name; /* визначене iм’я */
@@ -56,15 +59,63 @@ int under(char *s){
     return -1;
 }
 
+int getword(char *word, int lim)
+{
+    int c;
+    char *w = word;
+    while (isspace(c = getch()))
+        ;
+    if(c=='"'){
+        while ((c = getch())=='"')
+            ;
+        while ((c = getch())!='"')
+            ;
+        while ((c = getch())=='"')
+            ;
+    }
+    if(c=='/'){
+        if((c=getch()) == '/'){
+            while ((c = getch())!='\n')
+                ;
+        }else if(c == '*'){
+            while ((c = getch())!='/')
+                ;
+        }else{
+            ungetch(c);
+        }
+    }
+    if(c=='#'){
+        c = getch();
+
+    }
+
+    if (c != EOF)
+        *w++ = c;
+    if (!isalnum(c)) {
+        *w = '\0';
+        return c;
+    }
+    for ( ; --lim > 0; w++)
+        if (!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    *w = '\0';
+    return word[0];
+}
 int main()
 {
-    install("1", "11");
-    install("2", "22");
-    install("4", "44");
-    install("5", "55");
-    install("6", "66");
-    install("7", "77");
-    under("7");
+    char word[MAXWORD];
+
+    while (getword(word, MAXWORD) != EOF)
+        if (strcmp(word,"define\0")==0){
+            getword(word, MAXWORD);
+            char* temp1 = strdup(word);
+            getword(word, MAXWORD);
+            char* temp2 = strdup(word);
+            install(temp1, temp2);
+            printf("added new element to hashtable\n");
+        }
     return 0;
 }
 
